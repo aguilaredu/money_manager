10a56024c454e4c6a8fedcb8d17d4e3a7f54f9d0
Eduardo Aguilar <git@eduaguilar.com>
2024-07-24 11:50:09 -0600

    Added run bash script

---
+++ run.bat
@@ -0,0 +1,11 @@
+@echo off
+REM Activate the virtual environment
+call venv\Scripts\activate.bat
+
+REM Run your Python script
+python -i src\python\main.py
+
+REM Deactivate the virtual environment
+deactivate
+
+pause
